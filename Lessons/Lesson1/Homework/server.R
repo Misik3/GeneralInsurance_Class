2897b62f7169e09a400f59fccccb7e5e7162e13b
Misik3 <michalarvai@gmail.com>
2018-04-23 00:43:27 +0200

    1. homework

--- Lessons/Lesson1/Homework/server.R
+++ Lessons/Lesson1/Homework/server.R
@@ -1,6 +1,17 @@
+library(shiny)
+library(ggplot2)
 
+getwd()
+dt_KPI <- read.csv(".../GeneralInsurance_Class/data/lesson2_KPI.csv")
 
 # Define a server for the Shiny app
-function(input, output) {
-
-}
+shinyServer(function(input, output) {
+  output$dt_KPI.plot<- renderPlot({
+    p<-ggplot(data = dt_KPI,
+              mapping = aes_string(x = dt_KPI$Premium, y = dt_KPI$Expenses, colour = input$col)
+    ) +
+      geom_point() +
+      geom_smooth()
+    p + labs(x="Premium",y="Expenses")
+  })
+})
--- Lessons/Lesson1/Homework/ui.R
+++ Lessons/Lesson1/Homework/ui.R
@@ -1,23 +1,24 @@
+library(shiny)
 
-
+dt_KPI <- read.csv(".../GeneralInsurance_Class/data/lesson2_KPI.csv")
 # Use a fluid Bootstrap layout
 fluidPage(    
   
   # Give the page a title
   titlePanel("Lesson 1 - Homework"),
-  
 
   sidebarLayout(      
     
-
+    # Define the sidebar with one input
     sidebarPanel(
-
+      selectInput("col", "Coloring var:",
+                  choices=c("Region","Unit","Segment","Business","Year")),
+      hr(),
+      helpText("Data from lesson2_KPI.csv")
     ),
-    
-    
+    # Create a spot for the barplot
     mainPanel(
-       
+      plotOutput("dt_KPI.plot")
     )
-    
   )
 )
